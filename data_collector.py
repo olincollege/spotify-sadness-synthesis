90e0055b237bcfe4d9ae29bbefde120ed79d8d0d
Kelsedilla <klynnmcclung@gmail.com>
2023-03-29 18:43:26 -0400

    Create two functions for album data
    function get_albums uses spotipy to get a list of songs from an album
    using the album url.
    
    get_all_albums uses get_albums to get a list of songs for each of
    mitski and phoebe's albums. it takes no arguments, and returns a list
    of song names for each album

--- data_collector.py
+++ data_collector.py
@@ -41,3 +41,46 @@ def get_all_playlists(path):
         playlists = [get_tracks(url.strip()) for url in f]
 
     return playlists
+
+
+def get_albums(url):
+
+    data = sp.album_tracks(url)
+    return [track["name"] for track in data["items"]]
+
+
+def get_all_albums():
+    laurel_hell = get_albums(
+        "https://open.spotify.com/album/4rcinMUHEWOxpIwJo2sf22?si=JDKtk3IFSACxhVdO5b3U_g"
+    )
+    be_the_cowboy = get_albums(
+        "https://open.spotify.com/album/42cH7mrkfljkqkxA2Ip9Xq?si=K1ZiPV-xQH2qC1pB1pBrhw"
+    )
+    puberty_2 = get_albums(
+        "https://open.spotify.com/album/4Coa8Eb9SzjrkwWEom963Q?si=YgaN4NZjSM-s-R3IDfCVjA"
+    )
+    bury_me_at_makeout_creek = get_albums(
+        "https://open.spotify.com/album/3I2KkX13lHXuYqfBjSOopo?si=ttcTCKKYQueCMFmkWVCANQ"
+    )
+    retired_from_sad = get_albums(
+        "https://open.spotify.com/album/7K4SuWzgUEweJScduBcC6f?si=gzQZriBiRdyik-qVWKf-Yg"
+    )
+    lush = get_albums(
+        "https://open.spotify.com/album/22MICAVuz34zzqm4Se5Lga?si=KVn0cj73SzCD0bfrCdJNMA"
+    )
+    stranger_in_the_alps = get_albums(
+        "https://open.spotify.com/album/5rcJ5xCMYYLCgGilFDKRZl?si=569_TrzdQWiHMuatIqRqbw"
+    )
+    punisher = get_albums(
+        "https://open.spotify.com/album/6Pp6qGEywDdofgFC1oFbSH?si=nzWaWIYkS2uW2IDxETQhow"
+    )
+    return [
+        laurel_hell,
+        be_the_cowboy,
+        puberty_2,
+        bury_me_at_makeout_creek,
+        retired_from_sad,
+        lush,
+        stranger_in_the_alps,
+        punisher,
+    ]
