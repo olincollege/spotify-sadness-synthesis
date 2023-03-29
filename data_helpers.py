fb0d8c68a3b1499fbff88a106f5ea31128f3788a
Kelsedilla <klynnmcclung@gmail.com>
2023-03-29 09:32:32 -0400

    simplify get_avg_ranking
    a lot of code in get_avg_ranking was the same as the function
    get_all_ranking, so call get_all_ranking within get_avg_ranking

--- data_helpers.py
+++ data_helpers.py
@@ -10,14 +10,15 @@ def make_row_list(row, df):
 
 
 def find_percentile(song, playlist, dictionary):
+
     rank = playlist.index(song) + 1
     length = len(playlist)
     percentile = rank / length
-    if song not in dictionary:
+    if song not in dictionary and song != None and song != "":
         dictionary[song] = []
         dictionary[song].append(percentile)
 
-    else:
+    elif song != None and song != "":
         dictionary[song].append(percentile)
     return dictionary
 
@@ -32,21 +33,6 @@ def find_avg_percent(dictionary):
     return dictionary
 
 
-def get_avg_ranking(df):
-
-    percent_dict = {}
-    for row in df:
-        playlist = make_row_list(row, df)
-        for i in playlist:
-            percent_dict = find_percentile(i, playlist, percent_dict)
-
-        if row == (len(df) - 1):
-            break
-
-    avg_percent = find_avg_percent(percent_dict)
-    return avg_percent
-
-
 def get_all_ranking(df):
     percent_dict = {}
     for row in df:
@@ -58,3 +44,18 @@ def get_all_ranking(df):
             break
 
     return percent_dict
+
+
+def get_avg_ranking(df):
+
+    percent_dict = get_all_ranking(df)
+    # percent_dict = remove_songs(percent_dict, 5) - add once function is made
+    avg_percent = find_avg_percent(percent_dict)
+    return avg_percent
+
+
+# def get_one_album(album, df):
+#     album_dict = {}
+#     for i in df:
+
+#     return
